a6a852a8a5320a2b108fae8bb7c82e8ac299dfcb
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-08-28 19:47:40 +0200

    basic classes with methods sketch add

--- src/main/java/com/codecool/FilePartReader.java
+++ src/main/java/com/codecool/FilePartReader.java
@@ -1,4 +1,21 @@
 package com.codecool;
 
 public class FilePartReader {
+
+
+    public FilePartReader() {
+
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
+
+    }
+
+    public String read(){
+
+    }
+
+    public String readLines(){
+
+    }
 }
--- src/main/java/com/codecool/FileWordAnalyzer.java
+++ src/main/java/com/codecool/FileWordAnalyzer.java
@@ -1,4 +1,34 @@
 package com.codecool;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileWordAnalyzer {
+    private final FilePartReader filePartReader;
+
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public ArrayList<String> getWordsOrderedAlphabetically (){
+
+    }
+
+    public ArrayList<String> getWordsContainingSubstring (String subString ) {
+
+    }
+
+    public ArrayList<String> getStringsWhichPalindromes () {
+
+    }
+
+    private Boolean isPalindrome(){
+
+    }
+
+    private String[] getWords() throws IOException {
+        String readText = filePartReader.readLines();
+        return readText.replaceAll("[^A-za-z ]", "").split("\\s+");
+    }
+
 }
