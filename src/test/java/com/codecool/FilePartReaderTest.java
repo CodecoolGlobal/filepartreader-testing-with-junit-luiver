bc6d6dd498f63e01fc5e84355a90eab0c6615b9b
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-08-29 17:55:28 +0200

    pom.xml update

---
+++ src/test/java/com/codecool/FilePartReaderTest.java
@@ -0,0 +1,18 @@
+package com.codecool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilePartReaderTest {
+
+    @org.junit.jupiter.api.Test
+    void setup() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readLines() {
+    }
+}
